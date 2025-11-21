62d5c0436b420024695ed610dd0a2d1095ee6341
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-11-20 17:54:49 -0800

    add force dynamic, vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
   console.log(posts);
