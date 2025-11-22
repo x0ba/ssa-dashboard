12d2c991b88243e90d4b5224d7939e7b741c15af
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-11-22 03:14:10 -0800

    add skeleton state to card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Card, CardContent } from "~/app/_components/ui/card";
-import { currentUser } from "@clerk/nextjs/server";
-import { Music } from "lucide-react";
 import { Suspense } from "react";
 import { RecentEventsSection } from "~/app/_components/sections/recent-events-section";
 import { RecentLinksSection } from "~/app/_components/sections/recent-links-section";
 import { Skeleton } from "~/app/_components/ui/skeleton";
+import { WelcomeSection } from "~/app/_components/sections/welcome-section";
+import { WelcomeCardSkeleton } from "~/app/_components/loading/welcome-card-skeleton";
+import { Card } from "~/app/_components/ui/card";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 function EventsCardSkeleton() {
   return (
@@ -39,28 +39,13 @@ function LinksCardSkeleton() {
   );
 }
 
-export default async function HomePage() {
-  const user = await currentUser();
-  const userName = user?.fullName;
-  const currentDate = new Date().toLocaleDateString();
-
+export default function HomePage() {
   return (
     <main className="p-6">
       <h1 className="text-4xl font-bold">Overview</h1>
-      <Card className="from-primary/10 via-primary/5 to-background border-primary/20 mt-4 bg-linear-to-r">
-        <CardContent className="flex flex-col items-start justify-between gap-4 p-6 sm:flex-row sm:items-center sm:p-10">
-          <div className="space-y-2">
-            <h2 className="text-primary flex items-center gap-2.5 text-3xl font-bold tracking-tight sm:text-4xl">
-              <Music width={36} height={36} /> Welcome to SSA,{" "}
-              {userName?.substring(0, userName.indexOf(" ")) ?? " "}
-              {"."}
-            </h2>
-            <p className="text-muted-foreground text-lg font-medium">
-              {currentDate}
-            </p>
-          </div>
-        </CardContent>
-      </Card>
+      <Suspense fallback={<WelcomeCardSkeleton />}>
+        <WelcomeSection />
+      </Suspense>
       <div className="mt-6 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <Suspense fallback={<EventsCardSkeleton />}>
           <RecentEventsSection />
